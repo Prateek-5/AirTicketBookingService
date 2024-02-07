8d166d14bdbfbf702b4a36fb44f90eb3680820d2
Prateek-5 <prateek.ku11@gmail.com>
2024-02-07 16:57:51 +0530

    initial config of the rabbit mq

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,13 +1,24 @@
 const {BookingService}=require('../services/index');
 const {StatusCodes}=require('http-status-codes')
+const {createChannel,publishMessage}=require('../utils/messageQueue')
+const { REMINDER_BINDING_KEY} =require('../config/serverConfig')
+
 const bookingservice=new BookingService();
-
-
 class BookingController{
     constructor(){
+        
 
     }
+    async sendMessageToQueue(req,res){
+        
+        const channel=await createChannel();
+        const data={message: 'Success'};
 
+        publishMessage(channel,REMINDER_BINDING_KEY,JSON.stringify(data));
+        return res.status(200).json({
+            message:'Successfully published the event'
+        })
+    }
 
     async create(req,res){
         try {
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,12 +2,13 @@ const express =require('express');
 
 const {BookingController}=require('../../controllers/index');
 
+
 const bookingcontroller=new BookingController();
 const router = express.Router();
 
 router.post('/bookings',bookingcontroller.create);
+router.post('/publish',bookingcontroller.sendMessageToQueue);
 
 
 
 module.exports=router;
-   
